fc06b98f32e1d2df8f3f59a81b228950b994eb05
A-Guidry <anthonyguidry1@me.com>
2016-09-02 11:44:30 -0700

    Zodiac app

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// $('div.dropdown').each(function() {
+//     var $dropdown = $(this);
+
+//     $("a.dropdown-link", $dropdown).click(function(e) {
+//       e.preventDefault();
+//       $div = $("div.dropdown-container", $dropdown);
+//       $div.toggle();
+//       $("div.dropdown-container").not($div).hide();
+//       return false;
+//     });
+
+// });
+    
+//   $('html').click(function(){
+//     $("div.dropdown-container").hide();
+//   });
+
+
+$("#zAquarius").click(function(){
+	$("#Aquarius").toggle()
+})
+$("#zAries").click(function(){
+	$("#Aries").toggle()
+})
+
+$("#zCancer").click(function(){
+	$("#Cancer").toggle()
+})
+$("#zCapricorn").click(function(){
+	$("#Capricorn").toggle()
+})
+$("#zGemini").click(function(){
+	$("#Gemini").toggle()
+})
+$("#zLeo").click(function(){
+	$("#Leo").toggle()
+})
+$("#zLibra").click(function(){
+	$("#Libra").toggle()
+})
+$("#zPisces").click(function(){
+	$("#Pisces").toggle()
+})
+$("#zSagittarius").click(function(){
+	$("#Sagittarius").toggle()
+})
+$("#zScorpio").click(function(){
+	$("#Scorpio").toggle()
+})
+$("#zTaurus").click(function(){
+	$("#Taurus").toggle()
+})
+$("#zVirgo").click(function(){
+	$("#Virgo").toggle()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
