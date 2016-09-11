ab072e54a4f46c2a8513e1bf98af69deb5ded229
AGuidry <anthonyguidry1@me.com>
2016-09-10 23:35:10 -0700

    Updated jQuery to hide previous sign info

--- js/main.js
+++ js/main.js
@@ -1,73 +1,77 @@
-// $('div.dropdown').each(function() {
-//     var $dropdown = $(this);
-
-//     $("a.dropdown-link", $dropdown).click(function(e) {
-//       e.preventDefault();
-//       $div = $("div.dropdown-container", $dropdown);
-//       $div.toggle();
-//       $("div.dropdown-container").not($div).hide();
-//       return false;
-//     });
-
-// });
-    
-//   $('html').click(function(){
-//     $("div.dropdown-container").hide();
-//   });
-
+$(document).ready(function(){
 
 $("#zAquarius").click(function(){
+	myFunction()
 	$("#Aquarius").toggle()
 })
 $("#zAries").click(function(){
+	myFunction()
 	$("#Aries").toggle()
 })
 
 $("#zCancer").click(function(){
+	myFunction()
 	$("#Cancer").toggle()
 })
 $("#zCapricorn").click(function(){
+	myFunction()
 	$("#Capricorn").toggle()
 })
 $("#zGemini").click(function(){
+	myFunction()
 	$("#Gemini").toggle()
 })
 $("#zLeo").click(function(){
+	myFunction()
 	$("#Leo").toggle()
 })
 $("#zLibra").click(function(){
+	myFunction()
 	$("#Libra").toggle()
 })
 $("#zPisces").click(function(){
+	myFunction()
 	$("#Pisces").toggle()
 })
 $("#zSagittarius").click(function(){
+	myFunction()
 	$("#Sagittarius").toggle()
 })
 $("#zScorpio").click(function(){
+	myFunction()
 	$("#Scorpio").toggle()
 })
 $("#zTaurus").click(function(){
+	myFunction()
 	$("#Taurus").toggle()
 })
 $("#zVirgo").click(function(){
+	myFunction()
 	$("#Virgo").toggle()
 })
 
+// moves the bottom icons down the page when a sign is clicked
+$(".dropdown-link").click(function(){
+	$(".row").animate({bottom:"-80%"},5)
+})
 
+var tags = document.getElementsByClassName("initiallyHidden")
 
+document.getElementsByClassName("dropdown-link").onclick = function() {
+myFunction()}
 
+function myFunction() {
+	// loop through all tags with class initiallyHidden
+	for (var i = 0; i < tags.length; i++) {
+		// check for any tag that has display:block
+		if(tags[i].style.display == "block"){
+			// change that tag to display:none
+			tags[i].style.display = "none"
+		}
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
+}) //End Document.ready
 
 
 
